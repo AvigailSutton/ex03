96eab1e5153bc30fb6facc95f445dc20017e6b29
326339066 <37326339066@mby.co.il>
2023-11-10 09:24:01 +0200

    fix

--- ex03/CustomMiddleware.cs
+++ ex03/CustomMiddleware.cs
@@ -14,7 +14,7 @@
         {
             Console.WriteLine("fffff");
             await request(httpContext);
-            await httpContext.Response.WriteAsync("    avigail rrrrrrr");
+            await httpContext.Response.WriteAsync("rrrrrrr");
         }
     }
 }
